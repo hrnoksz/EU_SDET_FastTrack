b4a6cba9fb6fe9ffe7328c4678a4db0a86120178
hrnoksz <harun1973@yahoo.com>
2022-09-04 23:36:39 +0300

    CheckLetter.java done

---
+++ src/day02_control_flow_statements/CheckLetter.java
@@ -0,0 +1,39 @@
+package day02_control_flow_statements;
+
+import java.util.Scanner;
+
+public class CheckLetter {
+
+    public static void main(String[] args) {
+
+        checkLetter();
+
+    }
+    public static void checkLetter(){
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter input: ");
+        String input = scanner.next().toLowerCase();
+
+        if(input.length() > 1){
+            System.out.println("ERROR, please provide one char!!!");
+            input = scanner.next().toLowerCase();
+        }
+        boolean letter = input.charAt(0) >= 'a' && input.charAt(0) <= 'z';
+        boolean vowel = input.equals("a") || input.equals("i") || input.equals("o")
+                || input.equals("u") || input.equals("e");
+
+        if(!letter){
+            System.out.println("Not a LETTER!!!");
+
+        }else if (vowel){
+            System.out.println("Input is vowel");
+
+        }else{
+            System.out.println("Input is consonant");
+
+        }
+
+    }
+
+}
